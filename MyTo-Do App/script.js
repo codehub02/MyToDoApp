e4d7a612e50c999357e15c6c37d3708231d5bc1f
codehub02 <142536752+codehub02@users.noreply.github.com>
2023-11-05 01:15:02 +0530

    Add files via upload

---
+++ MyTo-Do App/script.js
@@ -0,0 +1,40 @@
+const inpBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-Container");
+
+function addTask(){
+    if(inpBox.value === ''){
+        alert("You must write something!!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inpBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inpBox.value = "";
+    saveData();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+
+}
+showTask();
